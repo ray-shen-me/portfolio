b75ddfc942a5737c82228a65cb360d74e126f20a
Ray <rayshen071@gmail.com>
2024-01-28 17:07:03 -0500

    modified projects page + changed font + removed useless lines

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,28 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import * as React from "react";
-import {NextUIProvider} from "@nextui-org/react";
+import { NextUIProvider } from "@nextui-org/react";
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Ray Shen</title>
-        <link rel="icon" type = "image/x-icon" href = "/public/favicon.ico"></link>
+        <link rel="icon" type="image/x-icon" href="/public/favicon.ico"></link>
       </Head>
 
-      <main>
+      <main className={inter.className}>
         <h1 className={styles.title}>
           Ray Shen
         </h1>
         <h2 className={styles.names}>Student and Developer</h2>
         <div className={styles.bcontainer}>
-          <Link href = "/project"><button className={styles.buttons}>Projects</button></Link>
-          <a href = "https://github.com/rayyshen" target="_blank"><button className={styles.buttons}>Github</button></a>
-          <Link href = "/aboutme"><button className={styles.buttons}>About Me</button></Link>
+          <Link href="/project"><button className={styles.buttons}>Projects</button></Link>
+          <a href="https://github.com/rayyshen" target="_blank"><button className={styles.buttons}>Github</button></a>
+          <Link href="/aboutme"><button className={styles.buttons}>About Me</button></Link>
         </div>
       </main>
 
@@ -33,24 +36,6 @@ export default function Home() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
         }
         code {
           background: #fafafa;
--- pages/project.js
+++ pages/project.js
@@ -2,24 +2,26 @@ import Link from "next/link";
 import styles from '../styles/Project.module.css';
 import Head from 'next/head'
 import React from "react";
-import {useState} from 'react';
+import { useState } from 'react';
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
 
 export default function project() {
-  const[cards] = useState([
+  const [cards] = useState([
     {
-      title: 'Mango Chat',
+      title: ' 🥭 Mango Chat',
       image: './mangochat.png',
       text: 'Chat privately and send money securely with MangoChat. MangoChat is a decentralized and end-to-encrypted chat web application that is capable of sending $DESO coin. It was made within 24 hours during HackTJ 9.0 and won the best overall hack award 🥳!',
       github: 'https://github.com/notken12/mangochat',
     },
     {
-      title: 'Foody',
-      image:'./Foody-logos_transparent.png',
+      title: ' 🍴 Foody',
+      image: './Foody-logos_transparent.png',
       text: 'Barcode scanning app for quick nutritional information, improves food information accessibility with translation and text-to-speech features for wider accessibility. Supports the food pharmacy program. Made in 24 hours during HackTJ 10.0',
       github: 'https://github.com/rayyshen/foody'
     },
     {
-      title: 'Portfolio',
+      title: ' 👋 Portfolio',
       image: './portfolio.png',
       text: 'This website was made with Next.js and React, it was my first reactive web project and I learned a lot about front end web design. It\'s currently a work in progress and hosted on Google Cloud.',
       github: 'https://github.com/rayyshen/portfolio'
@@ -33,29 +35,30 @@ export default function project() {
         <title>Ray Shen</title>
         <link rel="icon" type="image/x-icon" href="/public/favicon.ico"></link>
       </Head>
+      <meta charset="UTF-8"></meta>
 
-
-      <div className={styles.cardbody}>
-        <section className={styles.cardsection}>
-          <div className={styles.cardcontainer}>
-            <h1 className={styles.bigtitle}>Projects</h1>
-            <div className={styles.cards}>
-              {
-                cards.map((card, i) =>
-                <div key={i} className = {styles.card}>
-                  <h3>{ card.title }</h3>
-                  <img className={styles.projectimage} src = {card.image}></img>
-                  <p>
-                    {card.text}
-                  </p>
-                  <a href={card.github} target="_blank"><button className={styles.btn}>Github</button></a>
-                </div>)
-              }
+      <main className={inter.className}>
+        <div className={styles.cardbody}>
+          <section className={styles.cardsection}>
+            <div className={styles.cardcontainer}>
+              <h1 className={styles.bigtitle}>Projects</h1>
+              <div className={styles.cards}>
+                {
+                  cards.map((card, i) =>
+                    <div key={i} className={styles.card}>
+                      <h3 className={styles.cardtitle}>{card.title}</h3>
+                      <img className={styles.projectimage} src={card.image}></img>
+                      <p className={styles.cardtitle}>
+                        {card.text}
+                      </p>
+                      <a href={card.github} target="_blank"><button className={styles.btn}>Github</button></a>
+                    </div>)
+                }
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
-
+          </section>
+        </div>
+      </main>
 
 
 
@@ -68,24 +71,6 @@ export default function project() {
           flex-direction: column;
           justify-content: center;
           align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
         }
         code {
           background: #fafafa;
